fb756871a91d7b94eadadc9ea131267df2b2a8b3
Павел <107019787+Pol888@users.noreply.github.com>
2022-11-08 21:45:52 +0500

    the work is in full swing

--- project/Program.cs
+++ project/Program.cs
@@ -1,6 +1,24 @@
 ﻿
 string [] array = {"Привет", "друзья", "как", "ваши", "дела", "?"};
+int n = 0;                                                                                    
+string [ ] newArray = new string[n];                                                 
+int indexRecNewArray = 0;
 
+for (int i = 0; i < array.Length; i++) // определяет размер нового массива;
+{
+    if (array[i].Length <= 3)
+    {
+        n ++;
+    }
+}
+
+for (int i = 0; i < array.Length; i++) // печать newArray;
+{
+    if (array[i].Length <= 3)
+    {
+        
+    }
+}
 
 
 
