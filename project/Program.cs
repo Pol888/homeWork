c9b16c76a47f3dc9358ed162627313921efa9640
Павел <107019787+Pol888@users.noreply.github.com>
2022-11-08 22:09:54 +0500

    work is done

--- project/Program.cs
+++ project/Program.cs
@@ -1,7 +1,7 @@
 ﻿
 string [] array = {"Привет", "друзья", "как", "ваши", "дела", "?"};
 int n = 0;                                                                                    
-string [ ] newArray = new string[n];                                                 
+                                                 
 int indexRecNewArray = 0;
 
 for (int i = 0; i < array.Length; i++) // определяет размер нового массива;
@@ -11,14 +11,16 @@ for (int i = 0; i < array.Length; i++) // определяет размер но
         n ++;
     }
 }
+string [] newArray = new string[n];
 
 for (int i = 0; i < array.Length; i++) // печать newArray;
 {
     if (array[i].Length <= 3)
     {
-        
+        newArray[indexRecNewArray] = array[i];
+        indexRecNewArray ++; 
     }
 }
 
-
+Console.WriteLine($"[{string.Join(", ", newArray)}]");
 
